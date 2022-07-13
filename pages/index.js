b4cf03051bc6c72a97b0794ded2ee4bd5570e035
Musa Akhmedov <musa.punhan@pm.me>
2022-07-13 20:26:06 +0200

    separated data flow in lib/data file

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,45 @@
-export default function Home() {
+import Link from 'next/link'
+import { portfolioAndBlogData } from '../lib/data'
+
+export const getStaticProps = async () => {
+    const data = await portfolioAndBlogData()
+    return {
+        props: {
+            data,
+        },
+    }
+}
+
+export default function Home({ data }) {
+    console.log(data)
     return (
         <div className='text-green-800 text-center text-3xl font-bold'>
             Hello!
+            <div>
+                {data.portfolios.map((project) => (
+                    <div key={project.slug}>
+                        <Link
+                            href={`/portfolio/${project.slug}`}
+                            className='text-2xl font-bold'
+                        >
+                            <a>{project.title}</a>
+                        </Link>
+                    </div>
+                ))}
+            </div>
+            <div>
+                {data.posts.map((post) => (
+                    <div key={post.slug}>
+                        <Link
+                            href={`blog/${post.slug}`}
+                            className='text-2xl font-bold'
+                        >
+                            <a>{post.title}</a>
+                        </Link>
+                        <div>{post.date}</div>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
