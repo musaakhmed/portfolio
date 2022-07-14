13619aa3c994aacf76b7b26dd9c8db20f4b62ea0
Musa Akhmedov <musa.punhan@pm.me>
2022-07-14 07:24:27 +0200

    code mockup to Hero and minor changer to header

--- components/Hero.js
+++ components/Hero.js
@@ -1,21 +1,72 @@
 const Hero = () => {
     return (
-        <div className='h-screen w-screen flex flex-row items-center justify-center'>
-            <div className=''>
-                <div class='mockup-code'>
-                    <pre data-prefix='$'>
-                        <code>npm i daisyui</code>
+        <section className='h-screen w-screen'>
+            <div className='max-w-2xl lg:max-w-6xl mx-auto flex-col flex h-full items-center justify-around'>
+                <div className='hidden md:block mockup-code text-sm md:text-base bg-[#000] bg-opacity-75'>
+                    <pre data-prefix='$' className='text-gray-600 font-Source'>
+                        <code>Hi, my name is</code>
                     </pre>
-                    <pre data-prefix='>' class='text-warning'>
-                        <code>installing...</code>
+                    <pre data-prefix='>' className='text-sun-400'>
+                        <code>Musa!</code>
                     </pre>
-                    <pre data-prefix='>' class='text-success'>
-                        <code>Done!</code>
+                    <pre></pre>
+                    <pre data-prefix='!' className='text-sun-600'>
+                        <code>
+                            I am a front-end developer based in
+                            <span className='text-sun-400'> Barcelona</span>!
+                        </code>
+                    </pre>
+                    <pre></pre>
+                    <pre></pre>
+                    <pre></pre>
+                    <pre data-prefix='info' className='text-gray-20 px-6'>
+                        <code className='px-7'>
+                            I code, shoot digital and analogue photography,
+                            <p className='px-20'>
+                                walk long distances and occasionally write.
+                            </p>
+                        </code>
+                    </pre>
+                    <pre data-prefix='event' className='text-gray-200 px-6'>
+                        <code className='px-7'>
+                            I build web projects in Javascript,{' '}
+                            <p className='px-20'>mainly ReactJS and Next.JS.</p>
+                        </code>
                     </pre>
                 </div>
+                <div className='md:hidden'>
+                    <div className='mockup-code bg-[#000] bg-opacity-75'>
+                        <pre data-prefix='$'>
+                            <code>Hi, my name is</code>
+                        </pre>
+                        <pre data-prefix='>' className='text-warning'>
+                            <code>Musa!</code>
+                        </pre>
+                        <pre></pre>
+                        <pre></pre>
+                        <pre></pre>
+                        <pre data-prefix='info' className='text-gray-200 p-4'>
+                            <code className=''>
+                                I code, shoot digital and
+                                <p className='pl-12'>analogue photography,</p>
+                                <p className='pl-12'>walk long distances</p>
+                                <p className='pl-12'>and occasionally write.</p>
+                            </code>
+                        </pre>
+                        <pre data-prefix='info' className='text-gray-200 p-4'>
+                            <code className=''>
+                                I build web projects
+                                <p className='pl-12'>in Javascript,</p>
+                                <p className='pl-12'>
+                                    mainly ReactJS and Next.JS.
+                                </p>
+                            </code>
+                        </pre>
+                    </div>
+                </div>
+                <div className='h-1/3'>test</div>
             </div>
-            <div></div>
-        </div>
+        </section>
     )
 }
 
