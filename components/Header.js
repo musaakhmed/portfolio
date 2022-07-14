c1bda47a4dcb25ef1e1b064dcedf10f980e10090
Musa Akhmedov <musa.punhan@pm.me>
2022-07-14 05:01:47 +0200

    header dark mode done

--- components/Header.js
+++ components/Header.js
@@ -2,9 +2,9 @@ import Link from 'next/link'
 
 const Header = () => {
     return (
-        <div className='sticky top-0 z-50 bg-gray-50 bg-opacity-90 dark:bg-oxford font-Roboto'>
+        <div className='sticky top-0 z-50 bg-gray-50 bg-opacity-90 dark:bg-oxford font-Source drop-shadow-2xl dark:shadow-sun-50 shadow-midnight'>
             <nav className='mx-auto max-w-4xl h-16 flex justify-end items-center text-gray-200'>
-                <ol className='list-[decimal-leading-zero] text-sm list-inside flex justify-evenly space-x-12 marker:text-sun-500 marker:font-mono marker:text-sm marker:tracking-tighter'>
+                <ol className='list-[decimal-leading-zero] text-sm list-inside flex justify-evenly space-x-12 marker:text-sun-500 marker:font-Source marker:text-sm marker:tracking-tighter'>
                     <Link href='/'>
                         <a>
                             <li className='hover:text-sun transition-all duration-200 ease-in-out'>
@@ -27,7 +27,7 @@ const Header = () => {
                         </a>
                     </Link>
                 </ol>
-                <button className='ml-12 px-6 py-2 text-sun border border-sun rounded bg-midnight bg-opacity-0 hover:bg-opacity-100 transition-all duration-100 font-light text-sm'>
+                <button className='ml-12 px-6 py-2 text-sun border border-sun rounded bg-[#fff] bg-opacity-0 hover:bg-opacity-30 transition-all duration-300  text-sm'>
                     Say Hi!
                 </button>
             </nav>
