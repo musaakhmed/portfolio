389cea9b6f7df92e09c4b6c80d28981b13452f93
Musa Akhmedov <musa.punhan@pm.me>
2022-07-14 06:08:20 +0200

    body layer for dark / light themes to global.css

--- components/Header.js
+++ components/Header.js
@@ -8,8 +8,8 @@ const Header = () => {
         { label: 'Blog', path: '/blog' },
     ]
     return (
-        <div className='sticky top-0 z-50 bg-gray-50 bg-opacity-90 dark:bg-oxford font-Source drop-shadow-2xl dark:shadow-sun-50 shadow-oxford'>
-            <nav className='mx-auto max-w-4xl h-16 flex justify-end items-center text-oxford dark:text-gray-200'>
+        <div className='sticky top-0 z-50 bg-gray-50 dark:bg-oxford font-Source  dark:shadow-sun-50 shadow-oxford'>
+            <nav className='mx-auto h-16 w-full flex justify-end items-center text-oxford dark:text-gray-200'>
                 <ol className='list-[decimal-leading-zero] text-sm list-inside flex justify-evenly space-x-12 marker:text-sun-500 marker:font-Source marker:text-sm marker:tracking-tighter'>
                     {nav.map((nav) => (
                         <Link href={nav.path}>
@@ -21,14 +21,14 @@ const Header = () => {
                         </Link>
                     ))}
                 </ol>
-                <button className='ml-12 px-6 py-2 text-sun border border-sun rounded bg-gray-200 dark:bg-[#fff] bg-opacity-0 hover:bg-opacity-30 transition-all duration-300  text-sm'>
+                <button className='ml-12 px-6 py-2 text-sun border border-sun rounded bg-gray-200 bg-opacity-0 hover:bg-opacity-30 dark:bg-sun-700 dark:bg-opacity-20 dark:hover:bg-opacity-50 transition-all duration-300  text-sm'>
                     Say Hi!
                 </button>
-                <label class='swap swap-rotate'>
-                    <input type='checkbox' className='hidden' />
+                {/* <label className='swap swap-rotate'>
+                    <input type='checkbox' />
 
                     <svg
-                        class='swap-on fill-current w-8 h-8'
+                        className='swap-on fill-current w-8 h-8'
                         xmlns='http://www.w3.org/2000/svg'
                         viewBox='0 0 24 24'
                     >
@@ -36,13 +36,13 @@ const Header = () => {
                     </svg>
 
                     <svg
-                        class='swap-off fill-current w-8 h-8'
+                        className='swap-off fill-current w-8 h-8'
                         xmlns='http://www.w3.org/2000/svg'
                         viewBox='0 0 24 24'
                     >
                         <path d='M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z' />
                     </svg>
-                </label>
+                </label> */}
             </nav>
         </div>
     )
