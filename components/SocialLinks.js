8941e0bd4e453e82dffa90987681a44458385c34
Musa Akhmedov <musa.punhan@pm.me>
2022-07-27 11:33:49 +0200

    svgs

--- components/SocialLinks.js
+++ components/SocialLinks.js
@@ -4,9 +4,10 @@ const SocialLinks = () => {
     return (
         <main className='sticky w-full lg:w-12 bottom-0 lg:pl-24 lg:h-full bg-oxford lg:bg-transparent'>
             <section className='flex lg:flex-col justify-evenly lg:justify-center items-center space-x-2 lg:space-y-6'>
-                <div>
+                <div className=''>
                     <a href='https://github.com/musapunhan' target='_blank'>
                         <div>{githubIcon}</div>
+                        {/* <div className='w-[3px]  transition-all h-[1.5px] my-2 bg-sun-500'></div> */}
                     </a>
                 </div>
                 <div>
