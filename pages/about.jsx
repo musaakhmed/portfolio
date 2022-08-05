f5748093556e0dbda50994821ada5ba405819662
Musa Akhmedov <musa.punhan@pm.me>
2022-08-05 16:16:38 +0200

    About page kinda done

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,7 +1,79 @@
-import React from 'react'
+import Image from 'next/image'
 
-function about() {
-    return <div>about</div>
+function About() {
+    return (
+        <div className='flex lg:flex-row flex-col py-16 items-center max-w-5xl space-y-4 justify-center mx-auto font-Roboto'>
+            <div className='w-1/3 flex items-center justify-center'>
+                <Image
+                    className='rounded hover:opacity-100 opacity-75 transition-all duration-200 ease-in'
+                    src='/assets/photos/portrait.jpeg'
+                    width='240px'
+                    height='240px'
+                    alt=''
+                />
+            </div>
+            <section className='flex flex-col space-y-4 lg:p-4 p-2 w-2/3'>
+                <h5 className='text-2xl text-sun-600 py-2'>
+                    Hi, my name is Musa!
+                </h5>
+                <p className=''>
+                    I'm a self-taught front-end developer based in Barcelona. I
+                    have started learning HTML and later PHP and MySQL back in
+                    2005{' '}
+                    <em className='text-gray-500 text-sm'>
+                        (I still remember you, PHPNuke and Invision Power
+                        Board!)
+                    </em>
+                    .
+                </p>
+                <p>
+                    After studying Law and Political Science in Brussels, I have
+                    worked in journalism, politics, high-level political event
+                    management, hospitality management and international
+                    education.
+                </p>
+                <p>
+                    I later decided to return to web development in March 2020
+                    by learning Javascript and transitioning to React JS and
+                    Next.JS later.
+                </p>
+                <p>
+                    I have lived in Azerbaijan, Belgium, Germany, the UK and
+                    currently enjoy my time in Barcelona, Spain.
+                </p>
+                <p>
+                    My passions are all things tech, reading, photography (both
+                    digital and analogue), travel and outdoor activities.
+                </p>
+                <p>
+                    Here are the technologies I have been working with for the
+                    last years:
+                </p>
+                <p className='flex flex-col font-Source text-sm text-gray-500'>
+                    <ol className='list-disc marker:text-sun-600 ml-8'>
+                        <li className='list-outside hover:list-inside transition duration-300 ease-in'>
+                            JavaScript (ES6+)
+                        </li>
+                        <li className='list-outside hover:list-inside transition-all duration-150 ease-in-out'>
+                            TypeScript
+                        </li>
+                        <li className='list-outside hover:list-inside transition-all duration-150 ease-in-out'>
+                            React JS
+                        </li>
+                        <li className='list-outside hover:list-inside transition-all duration-150 ease-in-out'>
+                            Next.JS
+                        </li>
+                        <li className='list-outside hover:list-inside transition-all duration-150 ease-in-out'>
+                            Tailwind CSS
+                        </li>
+                        <li className='list-outside hover:list-inside transition-all duration-150 ease-in-out'>
+                            SASS
+                        </li>
+                    </ol>
+                </p>
+            </section>
+        </div>
+    )
 }
 
-export default about
+export default About
