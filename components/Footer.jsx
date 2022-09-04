5e3291aa3d2caf38e15c8958af2249623e23f8a7
Musa Akhmedov <musa.punhan@pm.me>
2022-09-04 06:02:38 +0200

    footer -mb extended to up to lg

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <footer className=' bg-oxford p-4 pt-12 max-w-xl lg:max-w-5xl max-h-fit mb-16 md:-mb-96 flex flex-col justify-center items-end mx-auto text-right text-sm text-gray-600 font-Source'>
+        <footer className=' bg-oxford p-4 pt-12 max-w-xl lg:max-w-5xl max-h-fit mb-16 lg:-mb-96 flex flex-col justify-center items-end mx-auto text-right text-sm text-gray-600 font-Source'>
             <span className='tracking-tighter'>
                 Made with Next.JS, Tailwind CSS, GraphCMS and Lots of{' '}
                 <span className='text-sun text-xl'>&#10084;</span>
