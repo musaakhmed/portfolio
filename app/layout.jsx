62c2b075d07e05a8f7a4e6b01a8f34402dea7619
Musa Akhmedov <musa.punhan@pm.me>
2023-11-20 10:33:50 +0100

    chore: start the app directory for NextJS 14

---
+++ app/layout.jsx
@@ -0,0 +1,26 @@
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+import SocialLinks from '../components/SocialLinks'
+
+export const metadata = {
+    title: 'Musa Akhmedov - Portfolio',
+    description:
+        "Musa Akhemdov' personal web development portfolio with projects and a blog",
+}
+
+export default function RootLayout({
+    // Layouts must accept a children prop.
+    // This will be populated with nested layouts or pages
+    children,
+}) {
+    return (
+        <html lang='en'>
+            <body>
+                <Header />
+                {children}
+                <Footer />
+                <SocialLinks />
+            </body>
+        </html>
+    )
+}
