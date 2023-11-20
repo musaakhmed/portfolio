d42dcad2e4f3e29ab6b8c25c455eccd3684f1018
Musa <musa.akhmed@pm.me>
2023-11-20 14:53:34 +0100

    fix: add app directory to tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,7 @@
 module.exports = {
     darkMode: 'class',
     plugins: [require('daisyui'), require('@tailwindcss/typography')],
-    content: [
-        './pages/**/*.{js,ts,jsx,tsx}',
-        './app/**/*.{js,ts,jsx,tsx}',
-        './components/**/*.{js,ts,jsx,tsx}',
-    ],
+    content: ['./app/**/*.{js,ts,jsx,tsx}'],
     theme: {
         extend: {
             fontFamily: {
